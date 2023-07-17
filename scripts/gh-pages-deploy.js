f8184f94d2e14b9c40d8c6382ddf7b19cbab5108
Brendan Koh <brenstack62@gmail.com>
2023-07-18 02:55:45 +0800

    Change import syntax for deployment script

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
-const execa = require("execa");
-const fs = require("fs");
+import execa from "execa";
+import { existsSync } from "fs";
 (async () => {
   try {
     await execa("git", ["checkout", "--orphan", "gh-pages"]);
@@ -8,7 +8,7 @@ const fs = require("fs");
     console.log("Building started...");
     await execa("npm", ["run", "build"]);
     // Understand if it's dist or build folder
-    const folderName = fs.existsSync("dist") ? "dist" : "build";
+    const folderName = existsSync("dist") ? "dist" : "build";
     await execa("git", ["--work-tree", folderName, "add", "--all"]);
     await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
     console.log("Pushing to gh-pages...");
